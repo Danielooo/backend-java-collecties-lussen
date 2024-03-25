211e0f8ad36413ffbdfbc7f8a284328bfd776388
Danielooo <dvanraffe@gmail.com>
2024-03-25 17:45:49 +0100

    huiswerk uitwerkingen af

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,43 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import static java.lang.System.in;
 
 public class Main {
     public static void main(String[] args) {
+        int[] numeric = {1,2,3,4,5,6,7,8,9,0};
+        String[] alphabetic = {"een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"};
 
-        // Hier mag je je code schrijven voor de hoofd-opdracht
+        Translator translator = new Translator(numeric, alphabetic);
+
+        Boolean play = true;
+        String invalid = "ongeldige invoer";
+        Scanner scanner = new Scanner(in);
+
+
+//        if: number kleiner is dan 10, dan sla je de waarde van translator.translate(number) op in String result en print je "De vertaling van " + number + " is " + result.
+//else: anders dan print je ongeldig
+
+        while (play) {
+            System.out.println("Type 'x' om te stoppen \nType 'v' om te vertalen");
+            String input = scanner.nextLine();
+            if (input.equals("x")) {
+                play = false;
+            } else if (input.equals("v")) {
+                System.out.println("Voer een int waarde in om te vertalen.");
+                int number = scanner.nextInt();
+                scanner.nextLine();
+                if (number < 10) {
+                    String result = translator.translate(number);
+                    System.out.println("De vertaling van " + " number " + " is " + result);
+                } else {
+                    System.out.println(invalid);
+                }
+            } else {
+                System.out.println(invalid);
+            }
+        }
+        
+
 
 
     }
